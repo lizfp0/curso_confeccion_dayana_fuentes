2a0d38dc728fd4883b6a8f4e939b91a1b0552f39
Liz <dayanafp1999@hotmail.com>
2019-05-17 20:27:21 +0200

    json, datos, js

--- 06_elementos/js/app.js
+++ 06_elementos/js/app.js
@@ -1,3 +1,5 @@
+import dialogPolyfill from '../../node_modules/dialog-polyfill/index.js'
+
 function main() {
     document.querySelector('#btn-info').addEventListener('click', mostrar)
     document.querySelector('#btn-cerrar').addEventListener('click', cerrar)
@@ -13,17 +15,22 @@ function main() {
 function main2() {
     let modal =  document.querySelector('#dlg-sample')
 
+    dialogPolyfill.registerDialog(modal);
+    // Now dialog acts like a native <dialog>.  
+
     document.querySelector('#btn-info')
        .addEventListener('click', onClick)
     document.querySelector('#btn-cerrar')
        .addEventListener('click', onClick)
-
 
     function onClick() {
        if (!modal.open) {
           modal.open = false
         }
        modal.open = !modal.open
+       if (modal.open) {
+           modal.showModal();
+       }
     }
 }
 

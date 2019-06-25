50cb3f884dfa7da07417572833a5889f09b09f6f
Liz <dayanafp1999@hotmail.com>
2019-06-25 17:37:07 +0200

    js

---
+++ JS/04_funciones.js
@@ -0,0 +1,24 @@
+/*function calcularSC(x, y, ...z) {
+    console.log(z)
+    return (x*x) + (y*y)
+}
+calcularSC(2, 4) //20
+let a = 2
+let b = 4
+calcularSC(a, b)  //20
+let x = 2
+let y = 4
+//console.log(calcularSC(y, x) ) //20
+// console.log(calcularSC('pepe', 'juan'))  //NaN
+//console.log(calcularSC(y))
+//console.log(calcularSC())
+console.log(calcularSC(4, 'pepe', 8, 4 ,3)) */
+
+function calcularNumeros() {
+   for (let i = 0; i < array.length; i++) {
+       const calcularNumeros = array[i];
+       
+   }
+    return
+}  
+console.log()

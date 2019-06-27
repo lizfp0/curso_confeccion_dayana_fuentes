9b59b20533bde2d0931789224f043f5736d7ab0d
lizfp0 <liz@hotmail.com>
2019-06-27 19:07:48 +0200

    js

--- JS/04_funciones.js
+++ JS/04_funciones.js
@@ -1,24 +1,30 @@
-/*function calcularSC(x, y, ...z) {
-    console.log(z)
-    return (x*x) + (y*y)
-}
-calcularSC(2, 4) //20
-let a = 2
-let b = 4
-calcularSC(a, b)  //20
-let x = 2
-let y = 4
-//console.log(calcularSC(y, x) ) //20
-// console.log(calcularSC('pepe', 'juan'))  //NaN
-//console.log(calcularSC(y))
-//console.log(calcularSC())
-console.log(calcularSC(4, 'pepe', 8, 4 ,3)) */
+/**
+ * 
+ * @param  {array} aDatos 
+ * @returns {number, array}
+ */
 
-function calcularNumeros() {
-   for (let i = 0; i < array.length; i++) {
-       const calcularNumeros = array[i];
-       
-   }
-    return
-}  
-console.log()
+function calcularSC(...aDatos) {
+    let r = 0
+    let aCuadrados = []
+    for (let i = 0; i < aDatos.length; i++) {
+        const item = aDatos[i];
+        let x = item * item
+        aCuadrados.push(x)
+        r += x
+    }
+    // return [r, aCuadrados]
+    return {sumaTotal: r, aCuadrados: aCuadrados}
+}
+
+/* let sumaTotal
+let aCuadrados
+let aResultados = calcularSC(1, 5, 7, 8)
+sumaTotal = aResultados[0]
+aCuadrados = aResultados[1] */
+
+// let [sumaTotal, aCuadrados] = calcularSC(1, 5, 7, 8)
+let {aCuadrados, sumaTotal} = calcularSC(1, 5, 7, 8)
+
+console.log(sumaTotal)
+console.log(aCuadrados)

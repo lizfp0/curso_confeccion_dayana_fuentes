f1d5715415b2a03b1129e3bf491c6d8c5b8340ae
lizfp0 <liz@hotmail.com>
2019-07-01 18:45:48 +0200

    js

--- js_oop/03_cadenas.js
+++ js_oop/03_cadenas.js
@@ -7,4 +7,16 @@ let otraCadena = 'Es mejor usar comillas simples'
 let nuevaCadena = `Nuevo en ${version}` // Template String
 
 console.log(cadena, otraCadena, nuevaCadena)
+console.log(cadena.toUpperCase())
+
+
+console.log(cadena.length)
+console.log(cadena[100])
+
+/*for (let i = 0; i < cadena.length; i++) {
+    const item = cadena[i];
+    console.log(item)
+}
+*/
+
 

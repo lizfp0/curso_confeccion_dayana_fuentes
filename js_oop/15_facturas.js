43522b384318188ccf945ff57b1f5907686cf9e7
lizfp0 <liz@hotmail.com>
2019-07-05 18:26:31 +0200

    js

--- js_oop/15_facturas.js
+++ js_oop/15_facturas.js
@@ -31,4 +31,69 @@ Añadir al menos dos métodos públicos:
     o al menos el importe total de la factura
 */
 
+const formatoFactura = {
+    empresa: {
+        nombre: '',
+        direccion: '',
+        telefono: '',
+        nNIF: ''
+    },
+    cliente: {
+        nombre: '',
+        direccion: '',
+        telefono: '',
+        nNIF: ''
+    },
+    items: [
+
+    ],
+    nFactura: '',
+    nIVA: '',
+    formaPago: '',
+    fecha: new Date()
+}
+
+const Factura = {
+    empresa: {
+        nombre: 'Supermercado DIA',
+        direccion: 'Calle Miralrio, 9',
+        telefono: 695448024,
+        nNIF: 8927408
+    },
+    cliente: {
+        nombre: 'Dayana Fuentes',
+        direccion: 'Calle Lago Ercina, 6',
+        telefono: 695299238,
+        nNIF: 51023173
+    },
+    items: [
+        {descripcion: '', precio: 0, cantidad: 0}
+    ],
+    nFactura: '',
+    nIVA: '',
+    formaPago: '',
+    fecha: new Date()
+}
+
+
+
+const datosFactura = {
+    nFactura: 0023,
+    nIVA: 10,
+    descripProducto: ['arroz', 'fideos','soja','wasabi', 'salmon'],
+    preUniProducto: 3,
+    cantProducto: 2,
+    formPago: ['efectivo', 'tarjeta'],
+ pago: function (formaPago = ''){
+    if (this._comprobarFormaDePAgo(formaPago)) {
+        console.log(`El cliente pagará en ${this}`)
+    }
+ },
+    
+ cobrar: function(calcular) {
+    calcular.total = (calcular.cantProducto * calcular.preUniProducto)
+    console.log(calcular(calcular.total))
+  }
+}
+console.log(datosFactura)
 

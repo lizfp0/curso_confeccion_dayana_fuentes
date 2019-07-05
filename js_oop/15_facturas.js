76612d2e489dc3243e4b4ef1854cfa1531007138
lizfp0 <liz@hotmail.com>
2019-07-05 16:25:12 +0200

    datos factura

---
+++ js_oop/15_facturas.js
@@ -0,0 +1,34 @@
+/* EXTRUCTURA DE UN OBJETO (MODELO DE DATOS)
+Definir la estructura de un objeto que almacena una factura.
+
+Las facturas están formadas por:
+- La información de la propia empresa:
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF
+
+- La información del cliente
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF
+
+- Una lista de elementos, cada uno de los cuales dispone de:
+    - descripción
+    - precio por unidad
+    - cantidad
+
+- Otra información básica de la factura
+    - el número de factura
+    - tipo de IVA
+    - forma de pago
+    - la fecha
+
+Añadir al menos dos métodos públicos:
+- el que calcula el importe total de la factura
+- el que muestra por pantalla la factura
+    o al menos el importe total de la factura
+*/
+
+
